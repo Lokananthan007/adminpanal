8d9a2d3bf093e8626892ed873c95a5cb654b961c
Lokananthan007 <srilogu146@gmail.com>
2023-11-23 13:59:37 +0530

    3d commit

--- src/components/main/catalog/Add.js
+++ src/components/main/catalog/Add.js
@@ -10,13 +10,14 @@ function Add() {
       <div className="container d-block">
         <div className="card" style={{ width: "70%", height: "300px" }}>
           <div className="form ps-5 ms-5">
+          <table  style={{ width: "100%" }} action="/insert" method="POST" enctype="multipart/table-data">
           <label htmlFor="category">Category:</label><br/>
             <input 
               type="text" 
               name="category" 
               id="category"
               className="form-control mt-4 ms-5"
-              style={{ width: "60%",paddingLeft:'20px' }}
+              style={{ width: "60%",paddingLeft:'18px' }}
               placeholder="Category"
               required/><br/>
             <label htmlFor="subcategory">Subcategory:</label><br/>
@@ -25,7 +26,7 @@ function Add() {
               name="subcategory"
               id="category"
               className="form-control mt-4 ms-5"
-              style={{ width: "60%" }}
+              style={{ width: "60%",paddingLeft:'18px' }}
               placeholder="Subcategory"
             />
             
@@ -35,6 +36,7 @@ function Add() {
             >
               Submit
             </button>
+            </table>
           </div>
         </div>
       </div>
